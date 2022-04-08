183e4233669105b91876658739be1396ef44f408
slash-l <slash_mumu@163.com>
2022-04-08 22:46:19 +0800

    code init

---
+++ src/main/java/com/jfrog/maven/AppSimple.java
@@ -0,0 +1,11 @@
+package com.jfrog.maven;
+
+/**
+ * Hello world!
+ */
+public class AppSimple {
+    public static void main(String[] args) {
+        new AppSimple();
+        System.out.println("Hello World!");
+    }
+}
