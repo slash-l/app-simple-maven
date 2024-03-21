4daf83b4b388ece2753d084a1abd4d5010c4a4ff
slash-l <slash_mumu@163.com>
2024-03-21 23:12:30 +0800

    update AppSimple.java

--- src/main/java/com/jfrog/maven/AppSimple.java
+++ src/main/java/com/jfrog/maven/AppSimple.java
@@ -23,7 +23,7 @@ public class AppSimple {
         JSON jsonObject = JSON.parseObject(payload);
 
         logger.info(jsonObject);
-        logger.info("code end 321");
+        logger.info("code end 111");
     }
 
     public String sayHello(String input){
